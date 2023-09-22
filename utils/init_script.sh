0aec6244ece0e49f101acd2538f893db02f78eaa
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-09-21 17:10:45 -0700

    Added script

--- utils/init_script.sh
+++ utils/init_script.sh
@@ -4,7 +4,7 @@
 IMAGE_NAME="my-grpc-server"
 
 # Build the Docker image
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME -f ../Dockerfile .
 
 # Run the Docker container in detached mode (-d) and map port 50051
 docker run -d -p 50051:50051 $IMAGE_NAME
