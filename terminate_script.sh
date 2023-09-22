2e787a98b7e04718460f4028f23545269176cea8
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-09-21 19:50:20 -0700

    Added script

--- terminate_script.sh
+++ terminate_script.sh
@@ -4,5 +4,5 @@
 sudo nitro-cli terminate-enclave --all
 
 ## Stop and remove the Docker container 'alpine/socat'
-#docker stop $(docker ps -q --filter ancestor=alpine/socat)
-#docker rm $(docker ps -aq --filter ancestor=alpine/socat)
+docker stop $(docker ps -q --filter ancestor=alpine/socat)
+docker rm $(docker ps -aq --filter ancestor=alpine/socat)
